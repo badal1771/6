7662fbcce479214a0863e7d3e4862f61c0d1f279
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-09-07 08:06:33 -0400

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,27 @@
+
+/*drawing a star cube
+*****
+*****
+*****
+*****
+*****
+a complete square of star
+*/
+
+//library
+#include <iostream>
+using namespace std;
+//main function
+int main()
+{
+  // loop
+	for(int i = 0; i < 5; i++){
+// writing loop in a loop
+		for(int i = 0; i < 5; i++){
+		cout << "*";
+		}
+	cout << endl;
+	}
+return 0;
+}
+
