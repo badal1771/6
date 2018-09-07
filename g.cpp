bdea651412189417393fbc5915d1aed4914e94dc
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-09-07 08:13:53 -0400

    Add files via upload

---
+++ g.cpp
@@ -0,0 +1,46 @@
+/*to print the following pattern
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
+//library
+#include<iostream>
+using namespace std;
+//main function
+
+int main()
+{
+// loop
+ int i, j, k;
+for(i=1;i<=5;i++)
+{
+for(j=i;j<5;j++)
+{
+cout<<" ";
+}
+for(k=1;k<(i*2);k++)
+{
+cout<<"*";
+}
+cout<<"\n";
+}
+for(i=4;i>=1;i--)
+{
+for(j=5;j>i;j--)
+{
+cout<<" ";
+}
+for(k=1;k<(i*2);k++)
+{
+cout<<"*";
+}
+cout<<"\n";
+}
+return 0;
+}
