d2132f672d8b9644e93ecb1f616acce8b19a554b
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-11-16 03:12:21 +0530

    Update 6_qu7_a.cpp

--- 6_qu7_a.cpp
+++ 6_qu7_a.cpp
@@ -1,11 +1,11 @@
 //Write a user-defined function toUpper() that returns the uppercase of the inputChar received. Use call-by-value.
 #include<iostream>
 using namespace std;
-
+//
 char toUpper(int x)
 {
   int y;
-  for(int i=97;i<=122;i++)
+  for(int i=1;i<=32;i++)
 {
   if(x==i)
   {
@@ -19,6 +19,7 @@ int main()
 {
   char a ;
   cout << " Enter Any Alphabet To convert it into lowercase:";
+  //inputing a in it
   cin >> a;
   cout << " Uppercase of the entered character is: ";
   int x=int(a);
